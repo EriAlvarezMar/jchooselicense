7a5d06c49ab87badfb90104d1573b5996bbd2915
Saúl Piña <sauljabin@gmail.com>
2016-01-18 23:01:05 -0500

    Change license

--- jchooselicense/src/jchooselicense/model/Parameter.java
+++ jchooselicense/src/jchooselicense/model/Parameter.java
@@ -1,3 +1,12 @@
+/**
+ * Copyright (c) 2016 Saúl Piña <sauljabin@gmail.com>.
+ * 
+ * This file is part of JChooseLicense.
+ * 
+ * JChooseLicense is licensed under The MIT License.
+ * For full copyright and license information please see the LICENSE.txt file.
+ */
+
 package jchooselicense.model;
 
 import jchooselicense.base.Model;
--- jchooselicense/src/jchooselicense/view/ViewAbout.java
+++ jchooselicense/src/jchooselicense/view/ViewAbout.java
@@ -1,14 +1,10 @@
 /**
- * 
- * AppViewAbout.java
- * 
- * Copyright (c) 2014, Saúl Piña <sauljabin@gmail.com>.
+ * Copyright (c) 2016 Saúl Piña <sauljabin@gmail.com>.
  * 
  * This file is part of JChooseLicense.
  * 
  * JChooseLicense is licensed under The MIT License.
  * For full copyright and license information please see the LICENSE.txt file.
- *
  */
 
 package jchooselicense.view;
